0795e206bb642c35eb99f312a2bcac12fe7f88ea
Matthias Lodner <matthias@lodner.de>
2023-05-19 01:06:29 +0200

    Added color and rounded croners to lecture view.

--- lib/components/lecture/lectrue_row.dart
+++ lib/components/lecture/lectrue_row.dart
@@ -14,28 +14,37 @@ class LectureRow extends StatefulWidget {
 class _LectureRowState extends State<LectureRow> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 8),
-          child: Column(
-            children: [
-              Text(widget.lecture.title),
-              Text(widget.lecture.instructor),
-            ],
-          ),
-        ),
-        // TODO: Take max width with title and note or align edit iconbutton to the right.
-        IconButton(
-          icon: const Icon(Icons.edit),
-          tooltip: 'Bearbeiten',
-          onPressed: () async {
-            await Navigator.push(context, MaterialPageRoute(builder: (context) => LectureScreen(lecture: widget.lecture,)));
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+      child: Container(
+        decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+        color: widget.lecture.color,
+      ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Column(
+                children: [
+                  Text(widget.lecture.title),
+                  Text(widget.lecture.instructor),
+                ],
+              ),
+            ),
+            // TODO: Take max width with title and note or align edit iconbutton to the right.
+            IconButton(
+              icon: const Icon(Icons.edit),
+              tooltip: 'Bearbeiten',
+              onPressed: () async {
+                await Navigator.push(context, MaterialPageRoute(builder: (context) => LectureScreen(lecture: widget.lecture,)));
 
-            setState(() {});
-          },
+                setState(() {});
+              },
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
