bf3f894729d9bdbe7fe66ae146e64e9166d9c7a4
Matthias Lodner <matthias@lodner.de>
2023-05-19 02:06:16 +0200

    UI changes.

--- lib/components/todo/TodoRow.dart
+++ lib/components/todo/TodoRow.dart
@@ -15,44 +15,66 @@ class TodoRow extends StatefulWidget {
 class _TodoRowState extends State<TodoRow> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Checkbox(
-          checkColor: Colors.white,
-          value: widget.todo.done,
-          onChanged: (bool? value) async {
-            widget.todo.done = value!;
-            await TodoDatabaseHelper.updateTodo(widget.todo);
-            setState(() {});
-          },
-        ),
-        IconButton(
-          icon: const Icon(Icons.alarm),
-          tooltip: 'Benachrichtigung',
-          onPressed: () {
-            // TODO: Functionality
-            setState(() {});
-          },
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8),
-          child: Column(
-            children: [
-              Text(widget.todo.title),
-              Text(widget.todo.note),
-            ],
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 8, 8, 0),
+      child: Row(
+        children: [
+          Checkbox(
+            checkColor: Colors.white,
+            value: widget.todo.done,
+            onChanged: (bool? value) async {
+              widget.todo.done = value!;
+              await TodoDatabaseHelper.updateTodo(widget.todo);
+              setState(() {});
+            },
           ),
-        ),
-        // TODO: Take max width with title and note or align edit iconbutton to the right.
-        IconButton(
-          icon: const Icon(Icons.edit),
-          tooltip: 'Bearbeiten',
-          onPressed: () async {
-            await Navigator.push(context, MaterialPageRoute(builder: (context) => TodoScreen(todo: widget.todo,)));
-            setState(() {});
-          },
-        ),
-      ],
+          IconButton(
+            icon: const Icon(Icons.alarm),
+            tooltip: 'Benachrichtigung',
+            onPressed: () {
+              // TODO: Functionality
+              setState(() {});
+            },
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Column(
+                children: [
+                  FittedBox(
+                    child: Text(
+                      widget.todo.title,
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  const Divider(
+                    color: Colors.grey,
+                    thickness: 1 ,
+                    indent : 10,
+                    endIndent : 10,
+                  ),
+                  Text(
+                    widget.todo.note,
+                    textAlign: TextAlign.left,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // TODO: Take max width with title and note or align edit iconbutton to the right.
+          Align(
+            alignment: Alignment.centerRight,
+            child: IconButton(
+              icon: const Icon(Icons.edit),
+              tooltip: 'Bearbeiten',
+              onPressed: () async {
+                await Navigator.push(context, MaterialPageRoute(builder: (context) => TodoScreen(todo: widget.todo,)));
+                setState(() {});
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
