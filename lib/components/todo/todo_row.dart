5dda7b9255128374745ebb2a3de0be81f64be860
Matthias Lodner <matthias@lodner.de>
2023-06-09 18:41:21 +0200

    UI changes to todo row.

--- lib/components/todo/todo_row.dart
+++ lib/components/todo/todo_row.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:keep_on_track/data/model/lecture.dart';
 import 'package:keep_on_track/data/model/todo.dart';
 import 'package:keep_on_track/screens/todo/todo.dart';
+import 'package:keep_on_track/services/database/lecture.dart';
 import 'package:keep_on_track/services/database/todo.dart';
 import 'package:keep_on_track/services/notification_service.dart';
 import 'package:tuple/tuple.dart';
@@ -17,106 +19,159 @@ class TodoRow extends StatefulWidget {
 }
 
 class _TodoRowState extends State<TodoRow> {
+  Color backgroundColor = Colors.black12;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-      child: Container(
-        decoration: const BoxDecoration(
-          color: Colors.black12,
-          borderRadius: BorderRadius.all(Radius.circular(8)),
-        ),
-        child: Row(
-          children: [
-            Checkbox(
-              checkColor: Colors.white,
-              value: widget.todo.done,
-              onChanged: (bool? value) async {
-                widget.todo.done = value!;
-                await TodoDatabaseHelper.updateTodo(widget.todo);
-                setState(() {});
-              },
+      child: FutureBuilder<Lecture?>(
+        future: _getLecture(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if(snapshot.hasData) {
+            backgroundColor = snapshot.data!.color;
+          }
+
+          return Container(
+            decoration: BoxDecoration(
+              color: backgroundColor,
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
             ),
-            IconButton(
-              icon: widget.todo.notificationID != null ? const Icon(Icons.alarm) : const Icon(Icons.alarm_off),
-              tooltip: 'Benachrichtigung',
-              onPressed: () async {
-                if(widget.todo.alertDate != null) {
-                  if(widget.todo.notificationID == null) {
-                    Tuple2<bool, String> temp = await NotificationService().scheduleNotificationTodo(widget.todo);
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Checkbox(
+                    fillColor: MaterialStateProperty.resolveWith(getColor),
+                    checkColor: Colors.black,
+                    value: widget.todo.done,
+                    onChanged: (bool? value) async {
+                      widget.todo.done = value!;
+                      await TodoDatabaseHelper.updateTodo(widget.todo);
+                      setState(() {});
+                    },
+                  ),
+                  IconButton(
+                    icon: widget.todo.notificationID != null ? const Icon(Icons.alarm) : const Icon(Icons.alarm_off),
+                    color: ThemeData.estimateBrightnessForColor(backgroundColor) == Brightness.dark ?
+                    Colors.white :
+                    Colors.black,
+                    tooltip: 'Benachrichtigung',
+                    onPressed: () async {
+                      if(widget.todo.alertDate != null) {
+                        if(widget.todo.notificationID == null) {
+                          Tuple2<bool, String> temp = await NotificationService().scheduleNotificationTodo(widget.todo);
 
-                    if(temp.item1) {
-                      setState(() {
-                      });
+                          if(temp.item1) {
+                            setState(() {
+                            });
 
-                      const snackBar = SnackBar(content: Text('Benachrichtigung aktiviert.'));
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    } else {
-                      setState(() {
-                        widget.todo.notificationID = null;
-                      });
+                            const snackBar = SnackBar(content: Text('Benachrichtigung aktiviert.'));
+                            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                          } else {
+                            setState(() {
+                              widget.todo.notificationID = null;
+                            });
 
-                      final snackBar = SnackBar(content: Text(temp.item2));
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    }
-                  } else {
-                    await NotificationService().cancelTodoNotification(widget.todo);
+                            final snackBar = SnackBar(content: Text(temp.item2));
+                            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                          }
+                        } else {
+                          await NotificationService().cancelTodoNotification(widget.todo);
 
-                    setState(() {
-                      widget.todo.notificationID = null;
-                    });
+                          setState(() {
+                            widget.todo.notificationID = null;
+                          });
 
-                    const snackBar = SnackBar(content: Text("Benachrichtigung deaktiviert"));
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  }
-                } else {
-                  const snackBar = SnackBar(content: Text("Kein Datum für die Benachrichtigung gesetzt."));
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                }
-              },
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 8),
-                child: Column(
-                  children: [
-                    Text(
-                      widget.todo.title,
-                      textAlign: TextAlign.left,
+                          const snackBar = SnackBar(content: Text("Benachrichtigung deaktiviert"));
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        }
+                      } else {
+                        const snackBar = SnackBar(content: Text("Kein Datum für die Benachrichtigung gesetzt."));
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      }
+                    },
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: Column(
+                        children: [
+                          Text(
+                            widget.todo.title,
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: ThemeData.estimateBrightnessForColor(backgroundColor) == Brightness.dark ?
+                              Colors.white :
+                              Colors.black,
+                            ),
+                          ),
+                          Divider(
+                            color: ThemeData.estimateBrightnessForColor(backgroundColor) == Brightness.dark ?
+                            Colors.white :
+                            Colors.black,
+                            thickness: 1 ,
+                            indent : 10,
+                            endIndent : 10,
+                          ),
+                          Text(
+                            widget.todo.note,
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: ThemeData.estimateBrightnessForColor(backgroundColor) == Brightness.dark ?
+                              Colors.white :
+                              Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    const Divider(
-                      color: Colors.grey,
-                      thickness: 1 ,
-                      indent : 10,
-                      endIndent : 10,
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                      icon: const Icon(Icons.edit),
+                      color: ThemeData.estimateBrightnessForColor(backgroundColor) == Brightness.dark ?
+                      Colors.white :
+                      Colors.black,
+                      tooltip: 'Bearbeiten',
+                      onPressed: () async {
+                        await Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                            TodoScreen(
+                              todo: widget.todo,
+                              deleteTodo: widget.deleteTodo,
+                            )
+                        ));
+                        setState(() {});
+                      },
                     ),
-                    Text(
-                      widget.todo.note,
-                      textAlign: TextAlign.left,
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: IconButton(
-                icon: const Icon(Icons.edit),
-                tooltip: 'Bearbeiten',
-                onPressed: () async {
-                  await Navigator.push(context, MaterialPageRoute(builder: (context) =>
-                      TodoScreen(
-                        todo: widget.todo,
-                        deleteTodo: widget.deleteTodo,
-                      )
-                  ));
-                  setState(() {});
-                },
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
+  }
+
+  Future<Lecture?> _getLecture() async {
+    return await LectureDatabaseHelper.getByID(widget.todo.lectureID!);
+  }
+
+  Color getColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.selected,
+      MaterialState.focused,
+      MaterialState.pressed,
+    };
+
+    if (states.any(interactiveStates.contains)) {
+      return Colors.white;
+    }
+
+    return ThemeData.estimateBrightnessForColor(backgroundColor) == Brightness.dark ?
+    Colors.white :
+    Colors.black;
   }
 }
